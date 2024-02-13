4b8be15c8f14f80ccacf883036482c561c546def
Jerry <jerryjake227@gmail.com>
2024-02-13 14:38:47 +0100

    Updating my shoppingmoore website

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import './TailWind.css';
+import Navbar from './Components/Navbar/Navbar';
+import About from './pages/About';
+import OtherProduct from './pages/OtherProduct';
+import Cart from './pages/Cart';
+import Contact from './pages/Contact';
+import Hero from './pages/Hero';
+import { Newsletter } from './Components/NewsLetter/Newsletter';
+import { Footer } from './Components/Footer/Footer';
+import { ContextApi } from './contextApi/ContextApi'; // Import the context provider
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        {/* Wrap your Routes and components with the context provider */}
+        <ContextApi>
+          <Routes>
+            <Route path="/" index element={<Hero />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/other" element={<OtherProduct />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </ContextApi>
+        <Newsletter />
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// 
+// tailwind.config.js
+module.exports = {
+  purge: [],
+  darkMode: true,
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
+
+
